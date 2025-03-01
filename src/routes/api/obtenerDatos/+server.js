c20fac9c050d79f16ecfc03997dbe9a41b079d57
FROY33 <f90ymtz@gmail.com>
2025-02-28 22:08:47 -0600

    Update +server.js cambio de IP

--- src/routes/api/obtenerDatos/+server.js
+++ src/routes/api/obtenerDatos/+server.js
@@ -1,6 +1,6 @@
 export async function GET() {
 
-    let response = await fetch('http://13.58.249.92:8000/obtenerDatos')
+    let response = await fetch('http://18.227.26.255:8000/obtenerDatos')
     let datos = await response.json()
 
     return new Response(JSON.stringify(datos),{status: 200})
