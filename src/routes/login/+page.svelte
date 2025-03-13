2c0dbc26b80f8b9ca0ebd3ec11a13ac154731aff
grzlz <guillermo.rdzlpz@gmail.com>
2025-03-13 14:45:22 -0500

    Fix jwt generation

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -32,7 +32,7 @@
       }
 
       try {
-        const response = await fetch('/api/login', {
+        const response = await fetch('/api/autenticarUsuario', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ correo: email, password }),
